e7c65ec5d271d098d0ec16f5ca50afb3c9eb95e7
EC2 Default User <ec2-user@ip-172-31-39-189.ap-northeast-1.compute.internal>
2024-09-24 03:07:34 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
+  
+  
 
 
   validates :name, uniqueness: true, presence: true, length: { in: 2..20 }
-  
   validates:introduction, presence: false, length: { maximum: 50 }
 
   def get_profile_image(width, height)
@@ -20,6 +25,18 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   
+  get 'relationships/create'
+  get 'relationships/destroy'
+  get 'create/destroy'
   get 'home/about', to: 'homes#about', as: 'about'
   devise_for :users
   root to: "homes#top"
@@ -8,6 +11,10 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:create, :index, :show, :edit, :update]
+  resources :users, only: [:create, :index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    	get "followings" => "relationships#followings", as: "followings"
+    	get "followers" => "relationships#followers", as: "followers"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
